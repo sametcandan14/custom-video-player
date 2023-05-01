03fcbcf19ba94559b1de2abc5ee5588f15901bd1
Samet Candan <c._samet@hotmail.com>
2023-05-01 13:18:10 +0300

    readme added

---
+++ README.md
@@ -0,0 +1,3 @@
+<h1>Video Player Project with HTML,CSS and JS</h1>
+
+![](./video_player.gif)
--- script.js
+++ script.js
@@ -1,24 +1,149 @@
 const container = document.querySelector(".container"),
   mainVideo = container.querySelector("video"),
+  videoTimeline = container.querySelector(".video-timeline"),
   progressBar = container.querySelector(".progress-bar"),
   playPauseBtn = container.querySelector(".play-pause i"),
   skipForward = container.querySelector(".skip-forward i"),
   skipBackward = container.querySelector(".skip-backward i"),
-  volumeBtn = container.querySelector(".volume i");
+  volumeBtn = container.querySelector(".volume i"),
+  volumeSlider = container.querySelector(".left input"),
+  currentVidTime = container.querySelector(".current-time"),
+  videoDuration = container.querySelector(".video-duration"),
+  speedBtn = container.querySelector(".playback-speed span"),
+  picInPicBtn = container.querySelector(".pic-in-pic span"),
+  speedOptions = container.querySelector(".speed-options "),
+  fullscreenBtn = container.querySelector(".fullscreen i");
+let timer;
+
+const hideControls = () => {
+  if (mainVideo.paused) return;
+  timer = setTimeout(() => {
+    container.classList.remove("show-controls");
+  }, 3000);
+};
+
+hideControls();
+
+container.addEventListener("mousemove", () => {
+  container.classList.add("show-controls");
+  clearTimeout(timer);
+  hideControls();
+});
+
+const formatTime = (time) => {
+  let seconds = Math.floor(time % 60),
+    minutes = Math.floor(time / 60) % 60,
+    hours = Math.floor(time / 3600);
+  seconds = seconds < 10 ? `0${seconds}` : seconds;
+  minutes = minutes < 10 ? `0${minutes}` : minutes;
+  hours = hours < 10 ? `0${hours}` : hours;
+
+  if (hours == 0) {
+    return `${minutes}:${seconds}`;
+  }
+
+  return `${hours}:${minutes}:${seconds}`;
+};
 
 mainVideo.addEventListener("timeupdate", (e) => {
   let { currentTime, duration } = e.target;
   let percent = (currentTime / duration) * 100;
   progressBar.style.width = `${percent}%`;
+  currentVidTime.innerHTML = formatTime(currentTime);
+});
+
+videoDuration.innerText = formatTime(mainVideo.duration);
+
+// mainVideo.addEventListener("loadeddata", () => {
+//   console.log(e.target);
+//   videoDuration.innerText = e.target.duration;
+// });
+
+videoTimeline.addEventListener("click", (e) => {
+  let timelineWidth = videoTimeline.clientWidth;
+  mainVideo.currentTime = (e.offsetX / timelineWidth) * mainVideo.duration;
+});
+
+const draggableProgressBar = (e) => {
+  let timelineWidth = videoTimeline.clientWidth;
+  progressBar.style.width = `${e.offsetX}px`;
+  mainVideo.currentTime = (e.offsetX / timelineWidth) * mainVideo.duration;
+  currentVidTime.innerHTML = formatTime(mainVideo.currentTime);
+};
+
+videoTimeline.addEventListener("mousedown", () => {
+  videoTimeline.addEventListener("mousemove", draggableProgressBar);
+});
+
+container.addEventListener("mouseup", () => {
+  videoTimeline.removeEventListener("mousemove", draggableProgressBar);
+});
+
+videoTimeline.addEventListener("mousemove", (e) => {
+  const progressTime = videoTimeline.querySelector("span");
+  let offsetX = e.offsetX;
+  progressTime.style.left = `${offsetX}px`;
+  let timelineWidth = videoTimeline.clientWidth;
+  let percent = (e.offsetX / timelineWidth) * mainVideo.duration;
+  progressTime.innerText = formatTime(percent);
 });
 
 volumeBtn.addEventListener("click", () => {
   if (!volumeBtn.classList.contains("fa-volume-high")) {
     mainVideo.volume = 0.5;
-    volumeBtn.classList.replace("fa-volume-high", "fa-volume-xmark");
+    volumeBtn.classList.replace("fa-volume-xmark", "fa-volume-high");
   } else {
     mainVideo.volume = 0;
+    volumeBtn.classList.replace("fa-volume-high", "fa-volume-xmark");
   }
+
+  volumeSlider.value = mainVideo.volume;
+});
+
+volumeSlider.addEventListener("input", (e) => {
+  mainVideo.volume = e.target.value;
+
+  if (e.target.value == 0) {
+    volumeBtn.classList.replace("fa-volume-high", "fa-volume-xmark");
+  } else {
+    volumeBtn.classList.replace("fa-volume-xmark", "fa-volume-high");
+  }
+});
+
+speedBtn.addEventListener("click", () => {
+  speedOptions.classList.toggle("show");
+});
+
+speedOptions.querySelectorAll("li").forEach((option) => {
+  option.addEventListener("click", () => {
+    mainVideo.playbackRate = option.dataset.speed;
+    speedOptions.querySelector(".active").classList.remove("active");
+    option.classList.add("active");
+  });
+});
+
+document.addEventListener("click", (e) => {
+  if (
+    e.target.tagName !== "SPAN" ||
+    e.target.className !== "material-symbols-rounded"
+  ) {
+    speedOptions.classList.remove("show");
+  }
+});
+
+picInPicBtn.addEventListener("click", () => {
+  mainVideo.requestPictureInPicture();
+});
+
+fullscreenBtn.addEventListener("click", () => {
+  container.classList.toggle("fullscreen");
+
+  if (document.fullscreenElement) {
+    fullscreenBtn.classList.replace("fa-compress", "fa-expand");
+    return document.exitFullscreen();
+  }
+  fullscreenBtn.classList.replace("fa-expand", "fa-compress");
+  container.requestFullscreen();
 });
 
 skipBackward.addEventListener("click", () => {
